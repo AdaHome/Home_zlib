67cc859099b153eac2cad09d667fd255f29d383c
Johan <johaa1993@gmail.com>
2017-03-11 00:33:56 +0100

    test

--- src/ztest.adb
+++ src/ztest.adb
@@ -9,25 +9,38 @@ package body ztest is
 
    nul : Interfaces.C.char renames Interfaces.C.nul;
    Version : aliased Interfaces.C.char_array := "1.2.11" & nul;
-   Z_Stream_Size_Byte : constant Interfaces.C.int := Z_Stream'Size / System.Storage_Unit;
+   Z_Stream_Size_Byte : constant Interfaces.C.int := Z_Native_Stream'Size / System.Storage_Unit;
 
 
-   procedure Initialize_Inflate (Stream : in out Z_Stream; Windows_Size  : in Z_Windows_Size) is
+   procedure Initialize_Inflate (Stream : in out Z_Native_Stream; Windows_Size  : in Z_Windows_Size) is
       function inflateInit2_underscore
-        (strm        : in out Z_Stream;
+        (strm        : in out Z_Native_Stream;
          windowBits  : in Interfaces.C.int;
          version     : in Interfaces.C.char_array;
          stream_size : in Interfaces.C.int)
-      return Z_Code;
+      return Z_Status;
       pragma Import (C, inflateInit2_underscore, "inflateInit2_");
       use Ada.Assertions;
-      R : Z_Code;
+      R : Z_Status;
    begin
       R := inflateInit2_underscore (Stream, Windows_Size, Version, Z_Stream_Size_Byte);
-      Assert (R /= Z_Code_Version_Error, "Z_VERSION_ERROR. zlib library version is incompatible with the version assumed by the caller.");
-      Assert (R /= Z_Code_Memory_Error, "Z_MEM_ERROR. There was not enough memory.");
-      Assert (R /= Z_Code_Stream_Error, "Z_STREAM_ERROR. A parameters are invalid, such as a null pointer to the structure.");
-      Assert (R = Z_Code_Ok, "Return value is not Z_OK.");
+      Assert (R /= Z_Status_Version_Error, "Z_VERSION_ERROR. zlib library version is incompatible with the version assumed by the caller.");
+      Assert (R /= Z_Status_Memory_Error, "Z_MEM_ERROR. There was not enough memory.");
+      Assert (R /= Z_Status_Stream_Error, "Z_STREAM_ERROR. A parameters are invalid, such as a null pointer to the structure.");
+      Assert (R = Z_Status_Ok, "Return value is not Z_OK.");
+   end;
+
+   procedure Set_Next_Input (Stream : in out Z_Native_Stream; Item : in Ada.Streams.Stream_Element_Array) is
+      use Ada.Assertions;
+   begin
+      Stream.Input_Next := Item'Address;
+      Stream.Input_Available := Item'Length;
+   end;
+
+   procedure Set_Next_Output (Stream : in out Z_Native_Stream; Item : out Ada.Streams.Stream_Element_Array) is
+   begin
+      Stream.Output_Next := Item'Address;
+      Stream.Output_Available := Item'Length;
    end;
 
 
--- src/ztest.ads
+++ src/ztest.ads
@@ -1,34 +1,63 @@
 with Interfaces.C;
 with Interfaces.C.Strings;
 with System;
+with Ada.Streams;
+with Ada.Streams.Stream_IO;
 
 package ztest is
 
    subtype Z_Windows_Size is Interfaces.C.int;
 
-   type alloc_func is access function
+   type Z_Allocation_Function is access function
      (Opaque : System.Address;
       Items  : Interfaces.C.unsigned;
       Size   : Interfaces.C.unsigned)
       return System.Address;
 
-   type free_func is access procedure (opaque : System.Address; address : System.Address);
+   type Z_Free_Function is access procedure (opaque : System.Address; address : System.Address);
 
-   type Z_Stream is record
-      Next_In   : System.Address             := System.Null_Address; -- next input byte
-      Avail_In  : Interfaces.C.unsigned      := 0;                   -- number of bytes available at next_in
-      Total_In  : Interfaces.C.unsigned_long := 0;                   -- total nb of input bytes read so far
-      Next_Out  : System.Address             := System.Null_Address; -- next output byte should be put there
-      Avail_Out : Interfaces.C.unsigned      := 0;                   -- remaining free space at next_out
-      Total_Out : Interfaces.C.unsigned_long := 0;                   -- total nb of bytes output so far
-      msg       : Interfaces.C.Strings.chars_ptr;                    -- last error message, NULL if no error
-      state     : System.Address;                                    -- not visible by applications
-      zalloc    : alloc_func                 := null;                -- used to allocate the internal state
-      zfree     : free_func                  := null;                -- used to free the internal state
-      opaque    : System.Address;                                    -- private data object passed to zalloc and zfree
-      data_type : Interfaces.C.int;                                  -- best guess about the data type ascii or binary
-      adler     : Interfaces.C.unsigned_long;                        -- adler32 value of the uncompressed data
-      reserved  : Interfaces.C.unsigned_long;                        -- reserved for future use
+   type Z_Native_Stream is record
+      Input_Next : System.Address := System.Null_Address;
+      -- next input byte
+
+      Input_Available : Interfaces.C.unsigned := 0;
+      -- number of bytes available at next_in
+
+      Input_Total : Interfaces.C.unsigned_long := 0;
+      -- total nb of input bytes read so far
+
+      Output_Next : System.Address := System.Null_Address;
+      -- next output byte should be put there
+
+      Output_Available : Interfaces.C.unsigned := 0;
+      -- remaining free space at next_out
+
+      Output_Total : Interfaces.C.unsigned_long := 0;
+      -- total nb of bytes output so far
+
+      Message : Interfaces.C.Strings.chars_ptr;
+      -- last error message, NULL if no error
+
+      State : System.Address;
+      -- not visible by applications
+
+      Allocation_Function : Z_Allocation_Function := null;
+      -- used to allocate the internal state
+
+      Free_Function : Z_Free_Function := null;
+      -- used to free the internal state
+
+      opaque : System.Address;
+      -- private data object passed to zalloc and zfree
+
+      data_type : Interfaces.C.int;
+      -- best guess about the data type ascii or binary
+
+      adler : Interfaces.C.unsigned_long;
+      -- adler32 value of the uncompressed data
+
+      reserved : Interfaces.C.unsigned_long;
+      -- reserved for future use
    end record;
    -- struct z_stream_s
 
@@ -55,40 +84,44 @@ package ztest is
      );
    -- Allowed flush values; see deflate() and inflate() below for details.
 
-   type Z_Code is
+   type Z_Status is
      (
-      Z_Code_Version_Error,
-      Z_Code_Buffer_Error,
-      Z_Code_Memory_Error,
-      Z_Code_Data_Error,
-      Z_Code_Stream_Error,
-      Z_Code_ERRNO,
-      Z_Code_Ok,
-      Z_Code_Stream_End,
-      Z_Code_Need_Dict
+      Z_Status_Version_Error,
+      Z_Status_Buffer_Error,
+      Z_Status_Memory_Error,
+      Z_Status_Data_Error,
+      Z_Status_Stream_Error,
+      Z_Status_ERRNO,
+      Z_Status_Ok,
+      Z_Status_Stream_End,
+      Z_Status_Need_Dict
      );
-   for Z_Code'Size use Interfaces.C.int'Size;
-   for Z_Code use
+   for Z_Status'Size use Interfaces.C.int'Size;
+   for Z_Status use
      (
-      Z_Code_Version_Error => -6,
-      Z_Code_Buffer_Error => -5,
-      Z_Code_Memory_Error => -4,
-      Z_Code_Data_Error => -3,
-      Z_Code_Stream_Error => -2,
-      Z_Code_ERRNO => -1,
-      Z_Code_Ok => 0,
-      Z_Code_Stream_End => 1,
-      Z_Code_Need_Dict => 2
+      Z_Status_Version_Error => -6,
+      Z_Status_Buffer_Error => -5,
+      Z_Status_Memory_Error => -4,
+      Z_Status_Data_Error => -3,
+      Z_Status_Stream_Error => -2,
+      Z_Status_ERRNO => -1,
+      Z_Status_Ok => 0,
+      Z_Status_Stream_End => 1,
+      Z_Status_Need_Dict => 2
      );
    -- Return codes for the compression/decompression functions.
    -- Negative values are errors, positive values are used for special but normal events.
 
 
-   procedure Initialize_Inflate (Stream : in out Z_Stream; Windows_Size  : in Z_Windows_Size);
+   procedure Initialize_Inflate (Stream : in out Z_Native_Stream; Windows_Size  : in Z_Windows_Size);
 
 
-   function Inflate (Stream : in out Z_Stream; Flush : Z_Flush) return Z_Code;
+   function Inflate (Stream : in out Z_Native_Stream; Flush : Z_Flush) return Z_Status;
    pragma Import (C, Inflate, "inflate");
    -- Z_STREAM_END if the end of the compressed data has been reached and all uncompressed output has been produced.
+
+   procedure Set_Next_Input (Stream : in out Z_Native_Stream; Item : in Ada.Streams.Stream_Element_Array);
+   procedure Set_Next_Output (Stream : in out Z_Native_Stream; Item : out Ada.Streams.Stream_Element_Array);
+
 
 end ztest;
